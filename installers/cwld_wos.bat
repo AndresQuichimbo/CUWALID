d4dc1d42d7139278f8d8b1d77fe628da7a3ce5e1
Cornish_Leo <leocoote888@gmail.com>
2024-06-26 09:27:46 +0100

    Bug fix

--- installers/cwld_wos.bat
+++ installers/cwld_wos.bat
@@ -1,97 +1,67 @@
 @ECHO OFF
 
-REM Variable Definitions
-SET DIR=CUWALID
-SET CWD=%cd%
-SET MIN=%UserProfile%\miniconda3
-SET MIN_BIN=%MIN%\condabin
+REM var definition
+SET MIN=%UserProfile%\miniconda3\condabin\
 SET ENV=cwld
 SET ENJ=jptr
 SET SPY=spdr
 SET PYT=3.11.4
-REM The token below is going to be removed in the future
-SET TKN=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
 
 REM Function to check if Miniconda is installed
 :check_miniconda
-IF EXIST "%MIN_BIN%\conda.bat" (
+where conda >nul 2>nul
+IF %ERRORLEVEL% EQU 0 (
     ECHO Miniconda is already installed.
 ) ELSE (
     ECHO Miniconda is not installed. Installing Miniconda...
     GOTO install_miniconda
 )
+GOTO setup_conda_envs
 
 REM Function to install Miniconda
 :install_miniconda
-curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
-start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=1 /RegisterPython=0 /S /D=%MIN%
-DEL Miniconda3-latest-Windows-x86_64.exe
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o miniconda.exe
+start /wait "" miniconda.exe /S /D=%UserProfile%\miniconda3
+del miniconda.exe
 
-REM Add conda to PATH
-SET PATH=%PATH%;%MIN_BIN%
-GOTO check_miniconda
+REM add conda to PATH
+SET PATH=%UserProfile%\miniconda3\Scripts;%UserProfile%\miniconda3;%PATH%
+GOTO setup_conda_envs
 
-REM Check and install Miniconda if necessary
-CALL :check_miniconda
-
+REM Function to set up Conda environments
+:setup_conda_envs
 ECHO version downloaded:
-CALL conda --version
+call conda --version
 
-REM Update conda
-CALL conda update -y -n base -c conda-forge conda
+REM update conda
+call conda update -y -n base -c defaults conda
 ECHO version updated to:
-CALL conda --version
+call conda --version
 
-REM Create Conda environments
-CALL conda create -y -n %ENV%
-CALL conda create -y -n %ENJ%
-CALL conda create -y -n %SPY%
+REM conda config --set auto_activate_base false
 
-REM Move into cuwalid environment and install packages
-CALL conda activate %ENV%
-CALL conda install -y -c conda-forge python=%PYT% geopandas rioxarray dask landlab pointpats scikit-image pip-tools chardet tqdm cartopy metpy numba cmaps cmcrameri seaborn nb_conda_kernels spyder-kernels ipykernel basemap bottleneck
-CALL conda deactivate
+REM create the environments
+call conda create -y -n %ENV%
+call conda create -y -n %ENJ%
+call conda create -y -n %SPY%
 
-REM Move into jupyter environment and install packages
-CALL conda activate %ENJ%
-CALL conda install -y -c conda-forge python=%PYT% jupyterlab nb_conda_kernels
-CALL conda deactivate
+REM move into cuwalid
+call conda activate %ENV%
+call conda install -y -c conda-forge python=%PYT% geopandas rioxarray dask landlab pointpats scikit-image pip-tools chardet tqdm cartopy metpy numba cmaps cmcrameri seaborn nb_conda_kernels spyder-kernels ipykernel basemap bottleneck
+call conda deactivate
 
-REM Move into spyder environment and install packages
-CALL conda activate %SPY%
-CALL conda install -y -c conda-forge python spyder spyder-kernels ipython
-CALL conda deactivate
+REM move into jupyter
+call conda activate %ENJ%
+call conda install -y -c conda-forge python=%PYT% jupyterlab nb_conda_kernels
+call conda deactivate
 
-CALL conda env list
+REM move into spyder
+call conda activate %SPY%
+call conda install -y -c conda-forge python spyder spyder-kernels ipython
+REM call conda list
+call conda deactivate
 
-REM CLONING REPOs
-REM -------------
-mkdir %DIR%
-
-REM Jump into CUWALID
-cd %DIR%
-
-REM Clone CUWALID training
-curl -LJO https://%TKN%@github.com/AndresQuichimbo/CUWALID_training/archive/refs/heads/main.zip
-tar -xzf CUWALID_training-main.zip
-RENAME CUWALID_training-main CUWALID_training
-DEL /Q /S CUWALID_training-main.zip
-
-REM Clone DRYP
-curl -LJO https://%TKN%@github.com/AndresQuichimbo/DRYPv2.0.1/archive/refs/heads/main.zip
-tar -xzf DRYPv2.0.1-main.zip
-RENAME DRYPv2.0.1-main DRYP
-DEL /Q /S DRYPv2.0.1-main.zip
-
-REM Clone STORM
-curl -LJO https://%TKN%@github.com/feliperiosg/STORM3/archive/refs/heads/main.zip
-tar -xzf STORM3-main.zip
-RENAME STORM3-main STORM3
-DEL /Q /S STORM3-main.zip
-
-cd /D %CWD%
-
-dir %DIR%
+call conda env list
 
 ECHO:
 ECHO success!?
