88ea6c84cc2124b7d1f81188358df6e637ace5db
Cornish_Leo <leocoote888@gmail.com>
2024-07-01 13:11:39 +0100

    Moved input files to simpler location

--- CUWALID/run_DRYP.py
+++ CUWALID/run_DRYP.py
@@ -7,6 +7,6 @@ from models.DRYP.dryp.main_DRYP import run_DRYP
 import time
 startTime = time.time()
 
-run_DRYP("input/DRYP_Input/input_test.dmp")
+run_DRYP("../test_input/input_test.dmp")
 
 print ('time=', time.time() - startTime)
---
+++ test.py
@@ -0,0 +1,3 @@
+from CUWALID.models.DRYP.dryp.main_DRYP import run_DRYP
+
+run_DRYP("test_input/input_test.dmp")
