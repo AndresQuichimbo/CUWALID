8af20e51b6aa28dd66c7d98ec23050fdcdc5c94c
Cornish_Leo <leocoote888@gmail.com>
2024-07-09 11:27:44 +0100

    Fixed issue with package dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="CUWALID",
-    version="0.0.5", 
+    version="0.1.1", 
     author="Andrés Quichimbo, Manuel Rios Gaona, Dagmawi Teklu Asfaw",
     author_email="cootelf@cardiff.ac.uk",
     description="CUWALID (Climate into Useful Water And Land Information in Drylands)",
@@ -29,7 +29,8 @@ setup(
         'pip-tools',
         'chardet',
         'tqdm',
-        'cartopy',
+        'numpy',
+        'Cartopy',
         'metpy',
         'numba',
         'cmaps',      
