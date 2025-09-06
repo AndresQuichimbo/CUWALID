cc252b318c24cacf3091c31f010ec22a3ceb2f5a
Andres Quichimbo <44269393+AndresQuichimbo@users.noreply.github.com>
2025-09-06 17:54:47 +0100

    Bump version to 1.1.51 in setup.py
    Updated the package version from 1.1.50 to 1.1.51 to prepare for a new release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="cuwalid",
 
-    version="1.1.50", 
+    version="1.1.51", 
 
     author="Andrés Quichimbo, Manuel Rios Gaona, Dagmawi Teklu Asfaw, Leo Coote",
     author_email="cootelf@cardiff.ac.uk",
