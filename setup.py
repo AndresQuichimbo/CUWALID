35ba17085e47d7bf4a171c91c2d012f4caf821d6
Andres Quichimbo <44269393+AndresQuichimbo@users.noreply.github.com>
2025-12-09 17:55:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="cuwalid",
 
-    version="1.1.55",
+    version="1.1.56",
 
     author="Andrés Quichimbo, Manuel Rios Gaona, Dagmawi Teklu Asfaw, Leo Coote",
     author_email="cootelf@cardiff.ac.uk",
