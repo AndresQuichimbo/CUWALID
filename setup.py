9d9d3c4d83ef6e8a3e8b9d88572586c9117d962c
c1755103 <c1755103@aquifer.geol.cf.ac.uk>
2026-05-12 22:56:17 +0100

    version 1.1.62

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="cuwalid",
 
-    version="1.1.61",
+    version="1.1.62",
 
     author="Andrés Quichimbo, Manuel Rios Gaona, Dagmawi Teklu Asfaw, Leo Coote",
     author_email="cootelf@cardiff.ac.uk",
